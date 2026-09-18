9c88c694bfc8cf1996c18541a18ae621bdad22d7
Christoph <rieder.christoph@gmx.at>
2026-09-18 15:31:10 +0200

    Persist history compatibility settings

---
+++ include/history/HistorySettingsStore.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <Preferences.h>
+#include "HistorySettings.h"
+
+namespace multibus::history {
+
+class SettingsStore {
+public:
+    bool begin() {
+        return prefs_.begin("mbhistory", false);
+    }
+
+    void end() {
+        prefs_.end();
+    }
+
+    bool load(Settings& settings) const {
+        settings = Settings{};
+        settings.storageEnabled = prefs_.getBool(kStorageKey, false);
+        settings.retransmissionEnabled = prefs_.getBool(kRetransmissionKey, false);
+        settings.retransmissionIntervalSeconds =
+            prefs_.getUShort(kIntervalKey, kDefaultRetransmissionIntervalSeconds);
+        return validSettings(settings);
+    }
+
+    bool save(const Settings& settings) {
+        if (!validSettings(settings)) return false;
+        if (prefs_.putBool(kStorageKey, settings.storageEnabled) != sizeof(bool)) return false;
+        if (prefs_.putBool(kRetransmissionKey, settings.retransmissionEnabled) != sizeof(bool)) return false;
+        return prefs_.putUShort(kIntervalKey, settings.retransmissionIntervalSeconds) == sizeof(uint16_t);
+    }
+
+    void clear() {
+        prefs_.clear();
+    }
+
+private:
+    static constexpr const char* kStorageKey = "store";
+    static constexpr const char* kRetransmissionKey = "retrans";
+    static constexpr const char* kIntervalKey = "retry_s";
+    mutable Preferences prefs_;
+};
+
+} // namespace multibus::history
