79ab835e763623c312bd180687d5e5db5ce1e11c
Christoph <rieder.christoph@gmx.at>
2026-09-18 15:59:54 +0200

    Add history query interval setting

--- include/history/HistorySettings.h
+++ include/history/HistorySettings.h
@@ -7,16 +7,22 @@ namespace multibus::history {
 constexpr uint16_t kDefaultRetransmissionIntervalSeconds = 600;
 constexpr uint16_t kMinRetransmissionIntervalSeconds = 30;
 constexpr uint16_t kMaxRetransmissionIntervalSeconds = 1200;
+constexpr uint16_t kDefaultRetrievabilityIntervalSeconds = 60;
+constexpr uint16_t kMinRetrievabilityIntervalSeconds = 30;
+constexpr uint16_t kMaxRetrievabilityIntervalSeconds = 1200;
 
 struct Settings {
     bool storageEnabled = false;
     bool retransmissionEnabled = false;
     uint16_t retransmissionIntervalSeconds = kDefaultRetransmissionIntervalSeconds;
+    uint16_t retrievabilityIntervalSeconds = kDefaultRetrievabilityIntervalSeconds;
 };
 
 inline bool validSettings(const Settings& settings) {
     return settings.retransmissionIntervalSeconds >= kMinRetransmissionIntervalSeconds &&
-           settings.retransmissionIntervalSeconds <= kMaxRetransmissionIntervalSeconds;
+           settings.retransmissionIntervalSeconds <= kMaxRetransmissionIntervalSeconds &&
+           settings.retrievabilityIntervalSeconds >= kMinRetrievabilityIntervalSeconds &&
+           settings.retrievabilityIntervalSeconds <= kMaxRetrievabilityIntervalSeconds;
 }
 
 } // namespace multibus::history
